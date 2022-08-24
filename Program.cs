88911ee2d6526932316adb85e8870b27e27500b9
Nick <74838258+xadmin20@users.noreply.github.com>
2022-08-24 13:52:16 +0300

    Задача 10

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int x = new Random().Next(100, 1000);
+void new_prog(int num){
+     Console.WriteLine($"{num}");
+     string strnum = num.ToString();
+     Console.WriteLine($" {strnum[1]}");
+
+}
+
+new_prog(x);
