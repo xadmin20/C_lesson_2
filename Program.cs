ea56d4a0d9dd071d015c683ed44053dffc333172
Nick <74838258+xadmin20@users.noreply.github.com>
2022-08-24 13:54:49 +0300

    Задача 13

--- Program.cs
+++ Program.cs
@@ -1,14 +1,16 @@
-﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-// 456 -> 5
-// 782 -> 8
-// 918 -> 1
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+int x = new Random().Next(1, 999999);
 
-int x = new Random().Next(100, 1000);
 void new_prog(int num){
      Console.WriteLine($"{num}");
      string strnum = num.ToString();
-     Console.WriteLine($" {strnum[1]}");
-
+     if (num/100 == 0) Console.WriteLine($"третьей цифры нет");
+     else { Console.WriteLine($"третья цифра в числе {num} - [{strnum[2]}]");
+            Console.WriteLine($"                       {'↑'.ToString()}");
+     }
 }
 
 new_prog(x);
